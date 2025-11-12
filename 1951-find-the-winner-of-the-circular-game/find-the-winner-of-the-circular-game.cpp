d2ffc5b6fb109298361a83bb2f90486380631a24
Ashutosh Shukla <ashutosh.shukla18503@gmail.com>
2025-11-12 23:20:52 +0530

    Time: 7 ms (38.27%) | Memory: 8 MB (54.98%) - LeetSync

---
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+int winner(vector<bool>&person , int n , int index , int person_left , int k){
+    //base case 
+    if(person_left==1){
+        for(int i=0 ; i<n ; i++){
+            if(person[i]==0)
+            return i;
+        }
+    }
+    //find the position killed 
+    int kill = (k-1)%person_left;
+    while(kill--){
+        index=(index+1)%n;
+        while(person[index]==1){
+            index=(index+1)%n;
+        }
+    }
+    person[index]=1;
+    //next alive person prr jao 
+    while(person[index]==1){
+        index=(index+1)%n;
+    }
+    return winner(person , n , index , person_left-1 , k);
+}
+    int findTheWinner(int n, int k) {
+        vector<bool> person(n,0);
+        return winner(person , n , 0 , n , k)+1;
+    }
+};
