e47d39fa839389f37129de030e533ce021348c05
Ashutosh Shukla <ashutosh.shukla18503@gmail.com>
2026-03-26 23:33:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (94.42%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        unordered_map<char, int> freq;
-        int cnt = 0;
-        bool odd = false;
-
-        for (char ch : s) {
-            freq[ch]++;
-        }
-
-        for (auto& pair : freq) {
-            if (pair.second % 2 == 0) {
-                cnt += pair.second;
-            } else {
-                cnt += pair.second - 1;
-                odd = true;
+        vector<int>lower(26,0);
+        vector<int>upper(26,0);
+        for(int i=0 ; i<s.size() ; i++){
+            if(s[i]>='a'){
+                lower[s[i]-'a']++;
+            }
+            else{
+                upper[s[i]-'A']++;
             }
         }
+        int count=0;
+        int odd=0;
+        for(int i=0 ; i<26 ; i++){
+            if(lower[i]%2==0){
+                count=count+lower[i];
+            }
+            else{
+                count=count+(lower[i]-1);
+                odd=1;
+            }
 
-        if (odd) {
-            return cnt + 1;
+            if(upper[i]%2==0){
+                count=count+upper[i];
+            }
+            else{
+                count=count+(upper[i]-1);
+                odd=1;
+            }
         }
-        return cnt;
+        return count+odd;
     }
 };
