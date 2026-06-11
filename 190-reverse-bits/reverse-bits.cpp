760ac3c5b02880658c864b75d2c8703d6da610f9
Ashutosh Shukla <ashutosh.shukla18503@gmail.com>
2026-06-11 18:01:05 +0530

    Time: 5 ms (4.96%) | Memory: 8.5 MB (5.92%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-
-        uint32_t ans = 0;
-
+        string num1 = "";
         for(int i = 0; i < 32; i++) {
+            num1 += (n % 2) + '0';
+            n /= 2;
+        }
 
-            ans = (ans << 1) | (n & 1);
+        int ans = 0;
 
-            n = n >> 1;
+        for(char bit : num1) {
+            ans = ans * 2 + (bit - '0');
         }
 
         return ans;
