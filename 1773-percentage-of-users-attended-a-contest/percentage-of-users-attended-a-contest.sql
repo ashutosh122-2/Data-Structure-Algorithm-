77ae6875725c88b2fc6d29e71292f98292ae016e
Ashutosh Shukla <ashutosh.shukla18503@gmail.com>
2026-06-26 15:05:51 +0530

    Time: 1067 ms (34.27%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,9 @@
+SELECT DISTINCT
+    contest_id,
+    ROUND(
+        (COUNT(contest_id) * 100) / (SELECT COUNT(*) FROM Users),
+        2
+    ) AS percentage
+FROM Register
+GROUP BY contest_id
+ORDER BY percentage DESC, contest_id ASC;
